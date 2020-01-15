613b214633d072fef8b0bff439933303540a95fe
Ryan Zheng <ryan0225@foxmail.com>
2020-01-14 21:22:46 -0800

    clean-up

--- AgentBind-IMPACT/auto_run.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-for idx in range(40,50):
-    workdir = "/storage/pandaman/project/AgentBind-IMPACT/tmp-c%d/" %idx
-    resultdir = "/storage/pandaman/project/AgentBind-IMPACT/results-c%d/" %idx
-    cmd = "python AgentBind.py --workdir %s --resultdir %s" %(workdir, resultdir)
-    subprocess.check_call(cmd, shell=True)
-
